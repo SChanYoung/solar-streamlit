5edb3b8f5feba93bd66738b26226c1f1d48308b2
SChanYoung <cks00062622@gmail.com>
2025-11-21 13:14:48 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -234,7 +234,7 @@ with tab1:
                     live_df.set_index("Timestamp", inplace=True)
 
                     # 🔹 5분 단위 평균
-                    resampled = live_df["PV_P (W)"].resample("5T").mean().reset_index()
+                    resampled = live_df["PV_P (W)"].resample("15S").mean().reset_index()
 
                     # 그래프 갱신
                     fig.data[1].x = resampled["Timestamp"]
