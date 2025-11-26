074ed68e4ab84f77351ef403fb37f037c18654c9
SChanYoung <cks00062622@gmail.com>
2025-11-26 11:36:08 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -241,7 +241,7 @@ with tab1:
                     cutoff_time = latest_time - pd.Timedelta(minutes=5)
                     
                     fixed_df = live_df[live_df["Timestamp"] < cutoff_time]
-                    resampled = fixed_df["PV_P (W)"].resample("5T").mean().reset_index()
+                    resampled = fixed_df["PV_P (W)"].resample("1T").mean().reset_index()
 
                     # 그래프 갱신
                     fig.data[1].x = resampled["Timestamp"]
